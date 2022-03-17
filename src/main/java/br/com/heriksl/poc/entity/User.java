120f8a964d0eb64da339cfd9c22547c42df829ee
Herik dos Santos Lorenção <heriksantoslorencao@gmail.com>
2022-03-16 21:18:50 -0300

    Adicionando testes unitários

--- src/main/java/br/com/heriksl/poc/entity/User.java
+++ src/main/java/br/com/heriksl/poc/entity/User.java
@@ -1,18 +1,17 @@
 package br.com.heriksl.poc.entity;
 
 public class User {
-	private String name;
-	private Long age;
+	private final String name;
+	private final Long age;
 
 	public User( String name, Long age ) {
 		this.name = name;
 		this.age = age;
 	}
 
-	public void talk(boolean isHappy) {
+	public String talk(boolean isHappy) {
 		if (isHappy)
-			System.out.println("I have a nice day!");
-		else
-			System.out.println("I have a bad day :/");
+			return "I have a nice day!";
+		return "I have a bad day :/";
 	}
 }
